b3c5ca9c027ccd7e5f23657bb63b012a2e9b9f1a
NitroInToaster <addons6@gmail.com>
2023-05-17 09:27:26 +0200

    put items

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
 
 class StudentCreateSchema(BaseModel):
     # name: str
@@ -8,6 +9,9 @@ class StudentCreateSchema(BaseModel):
     # tax: float | None = None
     first_name: str
     last_name: str
+
+# class StudentUpdateSchema(BaseModel):
+
 
 app = FastAPI()
 
@@ -19,3 +23,18 @@ async def root():
 @app.post("/items/")
 async def create_item(item: StudentCreateSchema):
     return item
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str):
+    return {"item_id": item_id}
+
+
+# @app.post("/items/{item_id}")
+# async def create_item(item: StudentCreateSchema):
+#     return item
+
+@app.put("/items/{item_id}", response_model=StudentCreateSchema)
+async def update_item(item_id: str, item: StudentCreateSchema):
+    update_item_encoded = jsonable_encoder(item)
+    items[item_id] = update_item_encoded
+    return update_item_encoded
